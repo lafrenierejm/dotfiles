7db7ab456ddc14f501882d0258371766bc67dfc5
Joseph LaFreniere <joseph@lafreniere.xyz>
2016-11-27 03:26:23 -0600

    Change `lla` to `la`

--- zsh/interactive/alias.zsh
+++ zsh/interactive/alias.zsh
@@ -4,7 +4,7 @@
 
 unalias -m '*' # remove all current aliases
 
-alias lla='ls -hlAF --color=auto' # human sizes, amost-all, colored
+alias la='ls -hlAF --color=auto' # human sizes, amost-all, colored
 alias mkdir='mkdir -p' # create parent directories as needed
 alias rmrf='rm -rf' # recurse, force
 alias termbin='nc termbin.com 9999' # send to termbin.com
