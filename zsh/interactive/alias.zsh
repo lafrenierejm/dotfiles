bf8fb8ca57669b8c3e969d31191d592f0f1f59a1
Joseph LaFreniere <joseph@lafreniere.xyz>
2016-12-17 15:45:48 -0600

    Add `mbsync` alias for $XDG_CONFIG_HOME compliance

--- zsh/interactive/alias.zsh
+++ zsh/interactive/alias.zsh
@@ -6,6 +6,7 @@ unalias -m '*' # remove all current aliases
 
 alias cp='cp -r' # copy directories recursively
 alias la='ls -hlAF --color=auto' # human sizes, amost-all, colored
+alias mbsync='mbsync -c "${HOME}/.config/isync/mbsyncrc" -a' # sync all channels
 alias mkdir='mkdir -p' # create parent directories as needed
 alias rmrf='rm -rf' # recurse, force
 alias scp='scp -r' # copy directories recursively
