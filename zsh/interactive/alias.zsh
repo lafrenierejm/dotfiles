0c8a0032c23e36f642061b204c92cb221844d9a4
Joseph LaFreniere <joseph@lafreniere.xyz>
2016-12-20 22:17:46 -0600

    zsh - Alias `ls` to output colors

--- zsh/interactive/alias.zsh
+++ zsh/interactive/alias.zsh
@@ -7,6 +7,7 @@ unalias -m '*' # remove all current aliases
 alias cp='cp -r' # copy directories recursively
 alias la='ls -hlAF --color=auto' # human sizes, amost-all, colored
 alias less='less -i' # case sensitive search only when term contains a capital
+alias ls='ls --color=auto' # colored
 alias mbsync='mbsync -c "${HOME}/.config/isync/mbsyncrc" -a' # sync all channels
 alias mkdir='mkdir -p' # create parent directories as needed
 alias mutt='mutt -F "${HOME}/.config/mutt/muttrc"'
