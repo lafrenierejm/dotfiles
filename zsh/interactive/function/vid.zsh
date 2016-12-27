55c7daa7fbd47abc72891a2228f0b6add6f6f76c
Joseph LaFreniere <joseph@lafreniere.xyz>
2016-12-27 09:49:33 -0600

    zsh - Update vid.zsh's options to use mpv

--- zsh/interactive/function/vid.zsh
+++ zsh/interactive/function/vid.zsh
@@ -5,7 +5,11 @@
 
 if [[ -n "${DISPLAY}" ]] && hash 'mpv' >/dev/null 2>&1; then
 	vid () {
-		mpv --volume 0 "$@" >/dev/null 2>&1 &
+		mpv \
+			--mute=yes \
+			"$@" \
+			>/dev/null 2>&1 \
+			&
 		disown
-	} >/dev/null
+	}
 fi
