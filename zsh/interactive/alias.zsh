3ce12aa2839990d35741f0910168446cbccfdf1a
Joseph LaFreniere <joseph@lafreniere.xyz>
2017-07-22 20:24:21 -0500

    [zsh] Add `clipboard` alias to copy to clipboard

--- zsh/interactive/alias.zsh
+++ zsh/interactive/alias.zsh
@@ -4,6 +4,7 @@
 
 unalias -m '*' # remove all current aliases
 
+alias clipboard='xclip -selection clipboard'
 alias cp='cp -r' # copy directories recursively
 alias la='ls -hlAF --color=auto' # human sizes, amost-all, colored
 alias less='less -i' # case sensitive search only when term contains a capital
