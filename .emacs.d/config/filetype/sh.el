6b48035fad466cd426819f97c26f4e07c56565ae
Joseph LaFreniere <joseph@lafreniere.xyz>
2018-01-05 13:25:00 -0600

    [emacs] Call `org-babel-tangle-file` directly to tangle config

--- .emacs.d/config/filetype/sh.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'auto-mode-alist
-	     '("\.z[^.]*\\'" . shell-script-mode))
-
-(add-hook 'shell-script-mode
-	  (lambda ()
-	    (if (string-match "\\.z[^.]*\\'" buffer-file-name)
-		(sh-set-shell "zsh"))))
