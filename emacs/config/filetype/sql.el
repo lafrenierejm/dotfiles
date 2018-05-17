b10d47612c46e52446edad61e562969a20a15b80
Joseph LaFreniere <joseph@lafreniere.xyz>
2018-05-16 21:18:08 -0500

    [emacs][sql] Remove disabled sqlup package

--- emacs/config/filetype/sql.el
+++
@@ -1,7 +0,0 @@
-;; Automatically capitalize SQL keywords
-(use-package sqlup-mode
-  :ensure t
-  :config
-  (add-hook 'sql-mode-hook 'sqlup-mode)
-  (add-hook 'sql-interactive-mode-hook 'sqlup-mode)
-  (add-hook 'redis-mode-hook 'sqlup-mode))
