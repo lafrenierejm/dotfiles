c913b77f02b73e48847512f6e618535337234687
Joseph LaFreniere <joseph@lafreniere.xyz>
2020-05-30 12:32:26 -0500

    [nixpkgs] Add initial home-manager configuration

---
+++ nixpkgs/service/lorri/default.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+  services.lorri = {
+    enable = true;
+  };
+}
---
+++ nixpkgs/service/mpd/default.nix
@@ -0,0 +1,10 @@
+{ config, pgks, ... }:
+
+{
+  services.mpd = {
+    enable = true;
+    musicDirectory = "${config.home.homeDirectory}/audio";
+    playlistDirectory = "${config.xdg.dataHome}/mpd/playlist";
+    dataDir = "${config.xdg.dataHome}/mpd";
+  };
+}
