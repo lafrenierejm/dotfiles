7a2f9614c752ac18252c937b86a2ce264d9d3f53
Joseph LaFreniere <joseph@lafreniere.xyz>
2021-03-30 19:17:44 -0500

    [guix][channels] Add github.com/flatwhatson channel

--- guix/channels.scm
+++ guix/channels.scm
@@ -10,4 +10,12 @@
  (channel
   (name 'lafrenierejm)
   (url "https://git.sr.ht/~lafrenierejm/lafrenierejm-guix"))
+ (channel
+  (name 'flat)
+  (url "https://github.com/flatwhatson/guix-channel")
+  (introduction
+   (make-channel-introduction
+    "33f86a4b48205c0dc19d7c036c85393f0766f806"
+    (openpgp-fingerprint
+     "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
  %default-channels)
