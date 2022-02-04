cfd1ba4c17247d849f7fdaad9ab6300b42c635da
Joseph LaFreniere <joseph@lafreniere.xyz>
2022-02-03 21:23:49 -0600

    [sh] Silence direnv logging

--- sh/0.env.sh
+++ sh/0.env.sh
@@ -1,5 +1,6 @@
 # Set generic environment variables
 
+export DIRENV_LOG_FORMAT=
 if [ -n "$INSIDE_EMACS" ]; then
     export PAGER='cat'
     export EDITOR="emacsclient --alternate-editor='' --no-wait"
