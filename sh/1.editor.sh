30cf4918f5b064a182db4cb3df0692713a714c14
Joseph LaFreniere <joseph@lafreniere.xyz>
2022-11-13 17:13:18 -0600

    [sh] Consolidate EDITOR and VISUAL config

--- sh/1.editor.sh
+++ sh/1.editor.sh
@@ -1,2 +1,4 @@
-export EDITOR="emacsclient --alternate-editor='' --tty %s"
-export VISUAL="emacsclient --alternate-editor='' --create-frame %s"
+if [ command -v emacs >/dev/null 2>&1 ]; then
+    export EDITOR="emacsclient --alternate-editor='' --tty %s"
+    export VISUAL="emacsclient --alternate-editor='' --create-frame %s"
+fi
