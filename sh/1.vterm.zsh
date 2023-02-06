fa78a444b6333f29502fde908f494d46da969bf1
Joseph LaFreniere <joseph@lafreniere.xyz>
2023-02-05 22:47:45 -0600

    [zsh][vterm] Print CWD relative to Git root, if any

--- sh/1.vterm.zsh
+++ sh/1.vterm.zsh
@@ -1,12 +1,27 @@
 if [[ "$INSIDE_EMACS" == "vterm" ]]; then
-    # https://github.com/akermu/emacs-libvterm#vterm-buffer-name-string
-    autoload -U add-zsh-hook
-    add-zsh-hook -Uz chpwd (){ print -Pn "\e]2;%m:%2~\a" }
+	# https://github.com/akermu/emacs-libvterm#vterm-buffer-name-string
+	autoload -U add-zsh-hook
+	add-zsh-hook -Uz chpwd () {
+		ROOT_DIR="$(git rev-parse --show-toplevel)"
+		if [ -n "$ROOT_DIR" ]; then
+			CWD="$(git rev-parse --show-prefix)"
+			if [ -z "$CWD" ]; then
+				TITLE="$(print -Pn '%1d')"
+			else
+				TITLE="$(basename $ROOT_DIR)/$CWD"
+			fi
+		else
+			TITLE="$(print -Pn '%~')"
+		fi
+		TITLE="${TITLE%/}"
+		export TITLE
+		print -Pn "\e]2;$TITLE\a"
+	}
 
-    # https://github.com/akermu/emacs-libvterm#directory-tracking-and-prompt-tracking
-    vterm_prompt_end() {
-	vterm_printf "51;A$(whoami)@$(hostname):$(pwd)";
-    }
-    setopt PROMPT_SUBST
-    PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+	# https://github.com/akermu/emacs-libvterm#directory-tracking-and-prompt-tracking
+	vterm_prompt_end() {
+		vterm_printf "51;A$(whoami)@$(hostname):$(pwd)";
+	}
+	setopt PROMPT_SUBST
+	PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
 fi
