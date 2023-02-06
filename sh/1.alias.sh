321e0858b05c452f5dd0d897d389ad0f4f267d26
Joseph LaFreniere <joseph@lafreniere.xyz>
2023-02-06 00:00:11 -0600

    [sh] If it is installed, use `exa` as `ls -l` replacement

--- sh/1.alias.sh
+++ sh/1.alias.sh
@@ -1,2 +1,7 @@
-alias ll='ls -lsh'
-alias la='ls -Alsh'
+if command -v exa 1>/dev/null 2>&1; then
+    alias ll='exa --long --git --time-style=long-iso'
+    alias la='exa --long --git --time-style=long-iso --all'
+else
+    alias ll='ls -lsh'
+    alias la='ls -Alsh'
+fi
