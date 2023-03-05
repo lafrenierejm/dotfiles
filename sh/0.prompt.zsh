c502e4bd391af5e43d2c732398ebe1ec98dc4ba0
Joseph LaFreniere <joseph@lafreniere.xyz>
2023-03-05 12:30:09 -0600

    Cleanup zsh config

--- sh/0.prompt.zsh
+++
@@ -1,6 +0,0 @@
-export PS1='%n@%m %1~ %# '
-if [ -z "$INSIDE_EMACS" ]; then
-    export PS2='%_>'
-else
-    export PS2=''
-fi
--- zsh/interactive/source-syntax-highlighting.zsh
+++
@@ -1,4 +0,0 @@
-# Source the zsh-syntax-highlighting script
-if [ -f '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ]; then
-	source '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
-fi
