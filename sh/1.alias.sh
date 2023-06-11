e10d47bc9c5d00dffcb0a391a80a6bc866488838
Joseph LaFreniere <git@lafreniere.xyz>
2023-06-10 20:36:12 -0500

    [zsh] Delete unused files

--- sh/1.alias.sh
+++
@@ -1,7 +0,0 @@
-if command -v exa 1>/dev/null 2>&1; then
-    alias ll='exa --long --git --time-style=long-iso'
-    alias la='exa --long --git --time-style=long-iso --all'
-else
-    alias ll='ls -lsh'
-    alias la='ls -Alsh'
-fi
