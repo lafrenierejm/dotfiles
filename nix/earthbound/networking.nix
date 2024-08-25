e19f1128d2a37e4e7188e0a019a164458071fed5
Joseph LaFreniere <git@lafreniere.xyz>
2024-08-24 21:51:04 -0400

    [nix] Simplify tailscale setup using Nix service options

--- nix/earthbound/networking.nix
+++ nix/earthbound/networking.nix
@@ -5,6 +5,8 @@
 }: {
   # Enable networking
   networking.hostName = "earthbound"; # Define your hostname.
+
+  # Enable wireless.
   networking.networkmanager.enable = true;
   networking.wireless.enable = false;
   networking.wireguard.enable = true;
@@ -14,40 +16,20 @@
   age.secrets.tailscale.file = ./tailscale.age;
   services.tailscale = {
     enable = true;
+    openFirewall = true;
     useRoutingFeatures = "client";
+    authKeyFile = config.age.secrets.tailscale.path;
+    extraUpFlags = [
+      "--exit-node=us-mia-wg-003.mullvad.ts.net"
+      "--exit-node-allow-lan-access"
+      "--accept-dns=true"
+    ];
   };
   environment.systemPackages = with pkgs; [
     tailscale
     tailscale-systray
     tailscalesd
   ];
-  ## The below service definition is copyright (c) Tailscale.
-  ## Its original source is https://tailscale.com/kb/1096/nixos-minecraft.
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatically connect to Tailscale";
-
-    # Ensure the tailscale service is running before attempting to connect.
-    after = ["network-pre.target" "tailscale.service"];
-    wants = ["network-pre.target" "tailscale.service"];
-    wantedBy = ["multi-user.target"];
-
-    serviceConfig.Type = "oneshot";
-
-    script = ''
-      # Wait for tailscaled to settle.
-      sleep 2
-
-      # If we're already authenticated, then do nothing.
-      status="$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then
-        exit 0
-      fi
-
-      # Otherwise, authenticate with tailscale.
-      "${pkgs.tailscale}/bin/tailscale" up --exit-node=us-mia-wg-003.mullvad.ts.net --exit-node-allow-lan-access --auth-key="file:${config.age.secrets.tailscale.path}"
-    '';
-  };
-
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [
