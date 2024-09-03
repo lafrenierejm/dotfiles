5e57baef10042f3dcbdd389f3b9c44a0f23d47fe
Joseph LaFreniere <git@lafreniere.xyz>
2024-09-02 23:58:23 -0400

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'disko':
        'github:nix-community/disko/511388d837178979de66d14ca4a2ebd5f7991cd3' (2024-08-29)
      → 'github:nix-community/disko/96073e6423623d4a8027e9739d2af86d6422ea7a' (2024-09-02)
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/af510d4a62d071ea13925ce41c95e3dec816c01d' (2024-08-30)
      → 'github:hercules-ci/flake-parts/567b938d64d4b4112ee253b9274472dc3a346eb6' (2024-09-01)
    • Updated input 'flake-parts/nixpkgs-lib':
        'https://github.com/NixOS/nixpkgs/archive/a5d394176e64ab29c852d03346c1fc9b0b7d33eb.tar.gz?narHash=sha256-uFf2QeW7eAHlYXuDktm9c25OxOyCoUOQmh5SZ9amE5Q%3D' (2024-08-01)
      → 'https://github.com/NixOS/nixpkgs/archive/356624c12086a18f2ea2825fed34523d60ccc4e3.tar.gz?narHash=sha256-Ss8QWLXdr2JCBPcYChJhz4xJm%2Bh/xjl4G0c0XlP6a74%3D' (2024-09-01)
    • Updated input 'homebrew-bundle':
        'github:homebrew/homebrew-bundle/88ecf2f0f8fe496c2f8994df4d820dd791aea69a' (2024-08-23)
      → 'github:homebrew/homebrew-bundle/5b019a719bfc87e0b80c4abd72ada660204ada13' (2024-09-02)
    • Updated input 'homebrew-cask':
        'github:homebrew/homebrew-cask/8caf71512efc74abb3b9e9fe359bc070d41b2dc4' (2024-09-01)
      → 'github:homebrew/homebrew-cask/6b2f8cfb4d42254bb5140469b2ccd1dafda6fc4e' (2024-09-03)
    • Updated input 'homebrew-core':
        'github:homebrew/homebrew-core/5b6a0db5e33fa8ffd276fc724621ff3b23531fde' (2024-09-01)
      → 'github:homebrew/homebrew-core/96da15ddbf9dafb2e2915a0fcc87e98fce818a76' (2024-09-03)
    • Updated input 'nixos-generators':
        'github:nix-community/nixos-generators/0dd0205bc3f6d602ddb62aaece5f62a8715a9e85' (2024-08-29)
      → 'github:nix-community/nixos-generators/8e337ce40421c4f33e7535aeb988d8e30fe067eb' (2024-09-02)
    • Updated input 'nixos-generators/nixlib':
        'github:nix-community/nixpkgs.lib/b741d900fecd2f0c32d90f853b24be9f5f098b7d' (2024-08-25)
      → 'github:nix-community/nixpkgs.lib/7f0b9e4fbd91826cb9ce6babbc11c87903191051' (2024-09-01)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/224e58da338d6d4525cacb754df6b8d6c40280d6' (2024-09-01)
      → 'github:NixOS/nixpkgs/cf72c84f471082c7febd08c7a2bac196b0b6b37c' (2024-09-03)
    • Updated input 'nixpkgs-unstable':
        'github:NixOS/nixpkgs/18051a0d7ab27caddda401992428181e1b04b49a' (2024-09-01)
      → 'github:NixOS/nixpkgs/2419b85a5eb75262b00a359c693d94b4aa9e880a' (2024-09-03)
    • Updated input 'ripgrep-all':
        'github:lafrenierejm/ripgrep-all/e207c1229f617672483f668826fe5ce92da57d45' (2024-06-24)
      → 'github:lafrenierejm/ripgrep-all/4a4c0928777a1b2d62c231b858f95078d96f05d1' (2024-09-01)
    • Updated input 'ripgrep-all/advisory-db':
        'github:rustsec/advisory-db/35e7459a331d3e0c585e56dabd03006b9b354088' (2024-04-27)
      → 'github:rustsec/advisory-db/fe4d5979b34444815287d61bd2a4e193cebbc7a6' (2024-08-27)
    • Updated input 'rust-overlay':
        'github:oxalica/rust-overlay/1fd72e343c6890f695243a37b367a1e3b90a49ee' (2024-09-01)
      → 'github:oxalica/rust-overlay/a562172c72d00350f9f2ff830e6515b6e7bee6d5' (2024-09-03)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/3ffd842a5f50f435d3e603312eefa4790db46af5' (2024-08-28)
      → 'github:numtide/treefmt-nix/9fb342d14b69aefdf46187f6bb80a4a0d97007cd' (2024-09-02)

--- flake.nix
+++ flake.nix
@@ -5,10 +5,7 @@
     nixpkgs.url = github:NixOS/nixpkgs/release-24.05;
     nixpkgs-unstable.url = github:NixOS/nixpkgs;
     agenix.url = github:ryantm/agenix;
-    crane = {
-      url = github:ipetkov/crane;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = github:ipetkov/crane;
     darwin = {
       url = github:lnl7/nix-darwin;
       inputs.nixpkgs.follows = "nixpkgs";
