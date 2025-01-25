3d1a2a5db717b4caa47dd70106b9ad3097aaa783
Joseph LaFreniere <git@lafreniere.xyz>
2025-01-24 20:20:27 -0500

    [hm] Reduce duplication in SSH config

--- nix/home.nix
+++ nix/home.nix
@@ -596,29 +596,26 @@ in rec {
       enable = true;
       compression = true;
       matchBlocks = {
+        "*" = {
+          host = "*";
+          identitiesOnly = true;
+          identityFile = "~/.ssh/id_ed25519";
+        };
         "github.com" = {
           hostname = "github.com";
           user = "lafrenierejm";
-          identityFile = "~/.ssh/id_ed25519";
-          identitiesOnly = true;
         };
         "gitlab.com" = {
           hostname = "gitlab.com";
           user = "lafrenierejm";
-          identityFile = "~/.ssh/id_ed25519";
-          identitiesOnly = true;
         };
         "git.sr.ht" = {
           hostname = "git.sr.ht";
           user = "lafrenierejm";
-          identityFile = "~/.ssh/id_ed25519";
-          identitiesOnly = true;
         };
         "earthbound" = {
           hostname = "10.0.0.53";
           user = "lafrenierejm";
-          identityFile = "~/.ssh/id_ed25519";
-          identitiesOnly = true;
         };
       };
     };
