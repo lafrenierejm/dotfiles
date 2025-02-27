e53687c62d19e3c3bfd92424741c792871bea31f
Joseph LaFreniere <git@lafreniere.xyz>
2025-02-26 21:53:11 -0500

    Deliberately track `.dir-locals.el`

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nix-mode . ((mode . nix-alejandra-on-save)))
+ (shell-script-mode . ((mode . indent-tabs)
+                       (tab-width . 4))))
