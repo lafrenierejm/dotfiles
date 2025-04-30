cded394f5c3554464dd2a814250f1b14aff83859
Joseph LaFreniere <git@lafreniere.xyz>
2025-04-30 10:11:36 -0400

    [emacs,zsh] Normalize directory tracking in shell-mode

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -11,16 +11,38 @@ short_pwd() {
 }
 
 # Copyright: (C) 2017-2020 by Lukas Fürmetz & Contributors
-# URL: https://github.com/akermu/emacs-libvterm#vterm-buffer-name-string
+# URL: https://github.com/akermu/emacs-libvterm#shell-side-configuration
 # License: GPL-3.0
-autoload -U add-zsh-hook
-add-zsh-hook -Uz chpwd () {
-	if [[ -n "$INSIDE_EMACS" ]]; then
-		export TITLE="$(short_pwd)"
-		print -Pn "\e]0;${TITLE}\a"
+vterm_printf() {
+	if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
+		# Tell tmux to pass the escape sequences through
+		printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+	elif [ "${TERM%%-*}" = "screen" ]; then
+		# GNU screen (screen, screen-256color, screen-256color-bce)
+		printf "\eP\e]%s\007\e\\" "$1"
+	else
+		printf "\e]%s\e\\" "$1"
 	fi
 }
+vterm_prompt_end() {
+	vterm_printf "51;A$(pwd)"
+}
 
+# Set prompt.
 setopt PROMPT_SUBST
 PROMPT='$(short_pwd) %(!.#.$) '
 RPROMPT=''
+
+# Tell Emacs what the working directory is.
+if [[ "$INSIDE_EMACS" == 'vterm' ]]; then
+	PROMPT+='%{$(vterm_prompt_end)%}'
+elif [[ "$INSIDE_EMACS" == 'ansi' ]]; then
+	chpwd() { print -P "\033AnSiTc %d" }
+
+	print -P "\033AnSiTu %n"
+	print -P "\033AnSiTc %d"
+elif [[ -n "$INSIDE_EMACS" ]]; then
+	chpwd() {
+		printf "\e]7;file://%s%s\e\\" "$(hostname)" "$(pwd)"
+	}
+fi
