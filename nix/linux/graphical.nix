d0f5fed909dcae56ea3d4856dd2f257514e529a4
Joseph LaFreniere <git@lafreniere.xyz>
2026-03-22 14:15:31 -0500

    [nix] Update path of `tuigreet` package

--- nix/linux/graphical.nix
+++ nix/linux/graphical.nix
@@ -31,7 +31,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
         user = "greeter";
       };
     };
