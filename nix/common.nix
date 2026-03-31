825907124b3b37430686c0f30fc16fa000c2865e
Joseph LaFreniere <git@lafreniere.xyz>
2026-03-30 23:56:23 -0500

    [nix::common] Update `nix.{registry,nixPath}` for consistent UX

--- nix/common.nix
+++ nix/common.nix
@@ -13,14 +13,13 @@
 in {
   age.secrets.cachix-auth.file = ./cachix-auth.age;
   nix = {
-    # # This will add each flake input as a registry
-    # # To make nix3 commands consistent with your flake
-    # registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-
     enable = true;
-    # # This will additionally add your inputs to the system's legacy channels
-    # # Making legacy nix commands consistent as well, awesome!
-    # nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    # Add each flake input as a registry to make `nix` subcommands consistent with the flake.
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+    # Add flake inputs to the system's legacy channels to make legacy `nix-*` commands consistent.
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
     package = pkgs.nixVersions.stable;
     settings =
       {
