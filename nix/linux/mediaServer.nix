4f04708000468c26d9d3a9a5b8d61c1ce307379b
Joseph LaFreniere <git@lafreniere.xyz>
2026-06-11 02:52:52 -0500

    [nix::mediaServer] Explicitly forward to IPv4 address

--- nix/linux/mediaServer.nix
+++ nix/linux/mediaServer.nix
@@ -99,7 +99,7 @@ in {
           lib.nameValuePair
           "/${service}"
           {
-            proxyPass = "http://localhost:${builtins.toString port}";
+            proxyPass = "http://127.0.0.1:${builtins.toString port}";
             proxyWebsockets = true;
           }
       )
