8ade2958f19858bdd18a7468d68e887bfee3a96e
Joseph LaFreniere <git@lafreniere.xyz>
2026-06-18 14:34:09 -0500

    [darkstar] Enable `fwupd`

--- nix/linux/darkstar/configuration.nix
+++ nix/linux/darkstar/configuration.nix
@@ -106,6 +106,8 @@
   #   enableSSHSupport = true;
   # };
 
+  services.fwupd.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
